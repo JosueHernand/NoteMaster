b387bc814e017ee63b681174c981e2091901a8bc
JosueHernand <joseh1337@gmail.com>
2023-10-24 02:35:43 -0700

    Added line of code to start the server

--- server.js
+++ server.js
@@ -10,3 +10,5 @@ app.use(express.json())
 
 require("./routes/apiRoutes/api")(app);
 require("./routes/htmlRoutes/html")(app);
+
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
