eab0cc94bfc284859ee99e9279529af834a7490f
JosueHernand <joseh1337@gmail.com>
2023-10-24 03:31:32 -0700

    Defined routes to retrieve the notes

--- routes/apiRoutes/api.js
+++ routes/apiRoutes/api.js
@@ -1,3 +1,9 @@
 const fs = require("fs");
 const { v4: uuid } = require('uuid');
 const path = require("path");
+
+module.exports = function(app) {
+    app.get("/api/notes", (req, res) => {
+
+    });
+};
